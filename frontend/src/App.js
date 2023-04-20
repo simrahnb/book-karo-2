a25773723887576492f12ca40b3edc04a1feb6a6
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-04-19 20:06:15 -0400

    Sidebar works - can now navigate to different pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route 
-              path="/"
+              path="/dashboard"
               element={<Dashboard />}
             />
             <Route
