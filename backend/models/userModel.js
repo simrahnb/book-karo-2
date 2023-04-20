78d9a1572fd8c78fab56e23f55f255ef3534cab2
simrahnb <65752120+simrahnb@users.noreply.github.com>
2023-04-20 01:21:14 -0400

    created user signup static method and hashed the passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema 
 
@@ -14,5 +15,23 @@ const userSchema = new Schema({
     }
 })
 
+// static signup method 
+userSchema.statics.signup = async function(email, password) {
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // mypasswordjds8909sj2cm
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash})
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
